279035b55d5a59c28e30fa1ea1d91e86bd85ddae
lujs <lujavascript@gmail.com>
2022-11-16 13:43:46 +0800

    feat: change template

--- src/generator/request.ts
+++ src/generator/request.ts
@@ -13,7 +13,23 @@ import {
   SourceFile,
 } from "ts-morph";
 import { Writer } from "./writer";
+type Alias = {
+  alias: string;
+};
 
+type PathAlias = {
+  name: string;
+  alias: Alias;
+}[];
+
+type QueryAlias = {
+  name: string;
+  alias: Alias;
+}[];
+
+type BodyAlias = Alias | null;
+
+type Response200Alias = Alias | null;
 export class RequestGenerator {
   constructor(public ast: RootAST) {}
 
@@ -26,155 +42,251 @@ export class RequestGenerator {
     if (requests.length) {
       await Promise.all(
         requests.map(async (s) => {
-          type Alias = {
-            alias: string;
-          };
-          let response200Alias: Alias | null = null;
-          if (s.responses?.length) {
-            const res200 = s.responses.filter((v) => v.status === 200)[0];
-            response200Alias = await this.writeSchema(
-              sf,
-              res200?.schema,
-              safeName(`Response_${s.id}`)
-            );
-          }
+          const { pathAlias, queryAlias, bodyAlias, response200Alias } =
+            await this.processPrams(sf, s);
 
-          const pathAlias: {
-            name: string;
-            alias: Alias;
-          }[] = [];
-          await Promise.all(
-            Object.entries(s?.pathParams || {})?.map(async ([name, schema]) => {
-              if (schema) {
-                const alias = await this.writeSchema(
-                  sf,
-                  schema,
-                  safeName(`PathParams_${s.id}_${name}`)
-                );
-                pathAlias.push({
-                  name,
-                  alias,
-                });
-              }
-            })
+          this.generateFunc(
+            s,
+            sf,
+            bodyAlias,
+            queryAlias,
+            pathAlias,
+            response200Alias
           );
-
-          const queryAlias: {
-            name: string;
-            alias: Alias;
-          }[] = [];
-          await Promise.all(
-            Object.entries(s?.queryParams || {})?.map(
-              async ([name, schema]) => {
-                if (schema) {
-                  const alias = await this.writeSchema(
-                    sf,
-                    schema,
-                    safeName(`PathParams_${s.id}_${name}`)
-                  );
-                  queryAlias.push({
-                    name,
-                    alias,
-                  });
-                }
-              }
-            )
-          );
-
-          let bodyAlias: Alias | null = null;
-          if (s.bodyParams) {
-            if (s.bodyParams.type === "json") {
-              bodyAlias = await this.writeSchema(
-                sf,
-                s.bodyParams.schema,
-                urlToMethodName(s.method + s.url + "Body", "pascal")
-              );
-            }
-          }
-
-          const clsSf = sf?.getClass("HttpService");
-
-          if (!clsSf?.getMethod(s.id)) {
-            // process parameter
-            let parameter: OptionalKind<ParameterDeclarationStructure>[] = [];
-            if (bodyAlias || queryAlias.length || pathAlias.length) {
-              parameter = [
-                {
-                  name: "parameter",
-                  type: (writer: CodeBlockWriter) => {
-                    writer.block(() => {
-                      if (bodyAlias) {
-                        writer.write(`body: ${bodyAlias.alias},`).endsWith(",");
-                      }
-                      if (queryAlias.length) {
-                        writer
-                          .write("params: ")
-                          .block(() => {
-                            queryAlias.forEach((v) => {
-                              writer.write(`'${v.name}': ${v.alias.alias},`);
-                            });
-                          })
-                          .endsWith(",");
-                      }
-                      if (pathAlias.length) {
-                        writer
-                          .write("path: ")
-                          .block(() => {
-                            pathAlias.forEach((v) => {
-                              writer.write(`'${v.name}': ${v.alias.alias},`);
-                            });
-                          })
-                          .endsWith(",");
-                      }
-                    });
-
-                    // console.log(writer.toString(), '=     writer.toString();');
-                  },
-                },
-              ];
-            }
-
-            clsSf?.addMethod({
-              isStatic: true,
-              name: s.id,
-              parameters: parameter,
-              statements: (writer) => {
-                writer.write("return Req.request");
-                if (response200Alias) {
-                  writer.write(`<${response200Alias.alias}>`);
-                }
-                writer.write("(");
-                writer
-                  .block(() => {
-                    if (pathAlias.length) {
-                      writer.writeLine(
-                        `url: replaceUrlPath('${s.url}', parameter?.path),`
-                      );
-                    } else {
-                      writer.writeLine(`url: '${s.url}',`);
-                    }
-
-                    writer.writeLine(`method: '${s.method}',`);
-                    if (queryAlias.length) {
-                      writer.writeLine(`params: parameter.params,`);
-                    }
-
-                    if (bodyAlias) {
-                      writer.writeLine(`data: parameter.body,`);
-                    }
-                  })
-                  .write(");");
-              },
-            });
-            // await sf.save();
-          } else {
-            console.error("方法已存在", s.id);
-          }
         })
       );
     }
 
     await sf.save();
+  }
+
+  async processPrams(sf: SourceFile, s: RequestAST) {
+    let response200Alias: Alias | null = null;
+    if (s.responses?.length) {
+      const res200 = s.responses.filter((v) => v.status === 200)[0];
+      response200Alias = await this.writeSchema(
+        sf,
+        res200?.schema,
+        safeName(`Response_${s.id}`)
+      );
+    }
+
+    const pathAlias: {
+      name: string;
+      alias: Alias;
+    }[] = [];
+    await Promise.all(
+      Object.entries(s?.pathParams || {})?.map(async ([name, schema]) => {
+        if (schema) {
+          const alias = await this.writeSchema(
+            sf,
+            schema,
+            safeName(`PathParams_${s.id}_${name}`)
+          );
+          pathAlias.push({
+            name,
+            alias,
+          });
+        }
+      })
+    );
+
+    const queryAlias: {
+      name: string;
+      alias: Alias;
+    }[] = [];
+    await Promise.all(
+      Object.entries(s?.queryParams || {})?.map(async ([name, schema]) => {
+        if (schema) {
+          const alias = await this.writeSchema(
+            sf,
+            schema,
+            safeName(`PathParams_${s.id}_${name}`)
+          );
+          queryAlias.push({
+            name,
+            alias,
+          });
+        }
+      })
+    );
+
+    let bodyAlias: Alias | null = null;
+    if (s.bodyParams) {
+      if (s.bodyParams.type === "json") {
+        bodyAlias = await this.writeSchema(
+          sf,
+          s.bodyParams.schema,
+          urlToMethodName(s.method + s.url + "Body", "pascal")
+        );
+      }
+    }
+
+    return {
+      bodyAlias,
+      queryAlias,
+      pathAlias,
+      response200Alias,
+    };
+  }
+
+  async generateCls(
+    s: RequestAST,
+    sf: SourceFile,
+    bodyAlias: BodyAlias,
+    queryAlias: QueryAlias,
+    pathAlias: PathAlias,
+    response200Alias: Response200Alias
+  ) {
+    const clsSf = sf?.getClass("HttpService");
+    if (!clsSf?.getMethod(s.id)) {
+      // process parameter
+      let parameter: OptionalKind<ParameterDeclarationStructure>[] = [];
+      if (bodyAlias || queryAlias.length || pathAlias.length) {
+        parameter = [
+          {
+            name: "parameter",
+            type: (writer: CodeBlockWriter) => {
+              writer.block(() => {
+                if (bodyAlias) {
+                  writer.write(`body: ${bodyAlias.alias},`).endsWith(",");
+                }
+                if (queryAlias.length) {
+                  writer
+                    .write("params: ")
+                    .block(() => {
+                      queryAlias.forEach((v) => {
+                        writer.write(`'${v.name}': ${v.alias.alias},`);
+                      });
+                    })
+                    .endsWith(",");
+                }
+                if (pathAlias.length) {
+                  writer
+                    .write("path: ")
+                    .block(() => {
+                      pathAlias.forEach((v) => {
+                        writer.write(`'${v.name}': ${v.alias.alias},`);
+                      });
+                    })
+                    .endsWith(",");
+                }
+              });
+              // console.log(writer.toString(), '=     writer.toString();');
+            },
+          },
+        ];
+      }
+      clsSf?.addMethod({
+        isStatic: true,
+        name: s.id,
+        parameters: parameter,
+        statements: (writer) => {
+          writer.write("return Req.request");
+          if (response200Alias) {
+            writer.write(`<${response200Alias.alias}>`);
+          }
+          writer.write("(");
+          writer
+            .block(() => {
+              if (pathAlias.length) {
+                writer.writeLine(
+                  `url: replaceUrlPath('${s.url}', parameter?.path),`
+                );
+              } else {
+                writer.writeLine(`url: '${s.url}',`);
+              }
+              writer.writeLine(`method: '${s.method}',`);
+              if (queryAlias.length) {
+                writer.writeLine(`params: parameter.params,`);
+              }
+              if (bodyAlias) {
+                writer.writeLine(`data: parameter.body,`);
+              }
+            })
+            .write(");");
+        },
+      });
+      // await sf.save();
+    } else {
+      console.error("方法已存在", s.id);
+    }
+  }
+
+  async generateFunc(
+    s: RequestAST,
+    sf: SourceFile,
+    bodyAlias: BodyAlias,
+    queryAlias: QueryAlias,
+    pathAlias: PathAlias,
+    response200Alias: Response200Alias
+  ) {
+    let parameter: OptionalKind<ParameterDeclarationStructure>[] = [];
+    if (bodyAlias || queryAlias.length || pathAlias.length) {
+      parameter = [
+        {
+          name: "parameter",
+          type: (writer: CodeBlockWriter) => {
+            writer.block(() => {
+              if (bodyAlias) {
+                writer.write(`body: ${bodyAlias.alias},`).endsWith(",");
+              }
+              if (queryAlias.length) {
+                writer
+                  .write("params: ")
+                  .block(() => {
+                    queryAlias.forEach((v) => {
+                      writer.write(`'${v.name}': ${v.alias.alias},`);
+                    });
+                  })
+                  .endsWith(",");
+              }
+              if (pathAlias.length) {
+                writer
+                  .write("path: ")
+                  .block(() => {
+                    pathAlias.forEach((v) => {
+                      writer.write(`'${v.name}': ${v.alias.alias},`);
+                    });
+                  })
+                  .endsWith(",");
+              }
+            });
+            // console.log(writer.toString(), '=     writer.toString();');
+          },
+        },
+      ];
+    }
+    sf?.addFunction({
+      isExported: true,
+      name: s.id,
+      parameters: parameter,
+      statements: (writer) => {
+        writer.write("return Req.request");
+        if (response200Alias) {
+          writer.write(`<${response200Alias.alias}>`);
+        }
+        writer.write("(");
+        writer
+          .block(() => {
+            if (pathAlias.length) {
+              writer.writeLine(
+                `url: replaceUrlPath('${s.url}', parameter?.path),`
+              );
+            } else {
+              writer.writeLine(`url: '${s.url}',`);
+            }
+            writer.writeLine(`method: '${s.method}',`);
+            if (queryAlias.length) {
+              writer.writeLine(`params: parameter.params,`);
+            }
+            if (bodyAlias) {
+              writer.writeLine(`data: parameter.body,`);
+            }
+          })
+          .write(");");
+      },
+    });
   }
 
   async writeSchema(
