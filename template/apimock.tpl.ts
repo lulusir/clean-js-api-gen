6a247a19ca0e6f7fd04173c4d0fcbcdd53595ce8
lujs <lujavascript@gmail.com>
2023-05-26 15:11:26 +0800

    feat: update mock

--- template/apimock.tpl.ts
+++ template/apimock.tpl.ts
@@ -4,3 +4,51 @@ import { Route } from 'worker-webserver';
 import { generateMock } from '@anatine/zod-mock';
 
 export const apiRoutes: Route[] = [];
+
+export function includePath(
+  apiRoutes: Route[],
+  includePaths: string[],
+): Route[] {
+  return apiRoutes.filter((r) => {
+    const path = r.path;
+
+    if (includePaths.includes(path)) {
+      return true;
+    }
+
+    for (const includePattern of includePaths) {
+      if (includePattern.endsWith('*')) {
+        const prefix = includePattern.slice(0, -1);
+        if (path.startsWith(prefix)) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  });
+}
+
+export function excludePath(
+  apiRoutes: Route[],
+  excludePaths: string[],
+): Route[] {
+  return apiRoutes.filter((r) => {
+    const path = r.path;
+
+    if (excludePaths.includes(path)) {
+      return false;
+    }
+
+    for (const excludePattern of excludePaths) {
+      if (excludePattern.endsWith('*')) {
+        const prefix = excludePattern.slice(0, -1);
+        if (path.startsWith(prefix)) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  });
+}
